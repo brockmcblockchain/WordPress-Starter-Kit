c2f929caed745ec15d9e4bbabad774652831168b
Robert Guss <robertguss@gmail.com>
2018-03-06 10:34:07 -0500

    the script is now working corretly 👍

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
-# WordPress installation scripts
+# WordPress install scripts
 
-a collection of bash scripts to automatically install and setup WordPress, using [WP-CLI](https://make.wordpress.org/cli/handbook/quick-start/), for local development with [Lando](https://docs.devwithlando.io/).
+This bash script automatically installs and creates a fresh WordPress install using [WP-CLI](https://make.wordpress.org/cli/handbook/) and [Lando](https://docs.devwithlando.io/) for local development.
+
+## How to install & use
+
+- Download & install [Docker](https://docs.docker.com/install/)
+- Download & install [Lando](https://docs.devwithlando.io/)
+
+```bash
+  # cd into this repo then run:
+  lando start
+
+  # this will start up lando and create the docker containers necessary for WordPress
+```
+
+- Once lando has been initialized you will see an output like the following:
+
+```bash
+BOOMSHAKALAKA!!!
+
+Your app has started up correctly.
+Here are some vitals:
+
+ NAME            my-word-press-site
+ LOCATION        /Users/robertguss/Projects/WP_Scripts
+ SERVICES        appserver, database
+
+ APPSERVER URLS  https://localhost:32786
+                 http://localhost:32787
+                 http://my-word-press-site.lndo.site:8000
+                 https://my-word-press-site.lndo.site:444
+```
+
+- Copy the 'APPSERVER URL' (usually the 3rd in the list) that looks similar to this: 'http://my-word-press-site.lndo.site:8000'
+
+- _You will need this address for the WordPress installation_
+
+```bash
+  # ssh into lando to run WP install script
+  lando ssh
+  bash install-wp.sh
+```
+
+- follow the prompts and enter in the required information.
---
+++ _install-wp.sh
@@ -0,0 +1,160 @@
+#!/bin/bash -e
+
+# wpuser='dnl-admin'
+
+clear
+
+echo "================================================================="
+echo "Awesome WordPress Installer!!"
+echo "================================================================="
+
+# accept user input for the databse name
+# echo "Database Name: "
+# read -e dbname
+
+# accept the name of the WP Admin User
+echo "WP Admin Username: (if left blank, defaults to: 'dnl-admin')"
+read -e wpuser
+
+# if the admin name is blank then default name is 'dnl-admin'
+if [ "$wpuser" == '' ] ; then
+wpuser='dnl-admin'
+fi
+
+# accept the email of the WP Admin User
+echo "WP Admin Email Address: "
+read -e adminemail
+
+# accept the name of our website
+echo "Site Name: "
+read -e sitename
+
+# accept a comma separated list of pages
+# echo "Add Pages: "
+# read -e allpages
+
+# ask the user to confirm the entered information is correct?
+echo "The WP Admin Username is: $wpuser"
+echo "The WP Admin Email Address is: $adminemail"
+echo "The Site Name is: $sitename"
+echo "Note: The WP Admin Password will be automatically generated, and printed later for you to copy"
+echo "If correct, would you like to run the install? (y/n)"
+read -e run
+
+# if the user didn't say no, then go ahead an install
+if [ "$run" == n ] ; then
+exit
+else
+
+# download the WordPress core files
+echo "Downloading the latest version of WordPress..."
+wp core download
+
+# create the wp-config file with our standard setup
+echo "Creating wp-config.php with debug settings enabled for development..."
+wp config create --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --dbhost=database --extra-php <<PHP
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_LOG', true );
+define( 'DISALLOW_FILE_EDIT', true );
+PHP
+
+# generate random 14 character password
+echo "Generating a random 14 digit strong password..."
+password=$(LC_CTYPE=C tr -dc A-Za-z0-9_\!\@\#\$\%\^\&\*\(\)-+= < /dev/urandom | head -c 14)
+
+# copy password to clipboard
+# echo $password | pbcopy
+
+# create database, and install WordPress
+# echo "Creating the database..."
+# wp db create
+
+echo "Installing WordPress..."
+wp core install --url="http://my-word-press-site.lndo.site:8000" --title="$sitename" --admin_user="$wpuser" --admin_password="$password" --admin_email="$adminemail"
+
+# discourage search engines
+echo "Disabling search engine indexing, remember to enable this when you go live!"
+wp option update blog_public 0
+
+# show only 6 posts on an archive page
+wp option update posts_per_page 6
+
+# delete sample page, and create homepage
+wp post delete $(wp post list --post_type=page --posts_per_page=1 --post_status=publish --pagename="sample-page" --field=ID --format=ids)
+
+wp post create --post_type=page --post_title=Home --post_status=publish --post_author=$(wp user get $wpuser --field=ID --format=ids)
+
+# set homepage as front page
+wp option update show_on_front 'page'
+
+# set homepage to be the new page
+wp option update page_on_front $(wp post list --post_type=page --post_status=publish --posts_per_page=1 --pagename=Home --field=ID --format=ids)
+
+# create all of the pages
+# export IFS=","
+# for page in $allpages; do
+# 	wp post create --post_type=page --post_status=publish --post_author=$(wp user get $wpuser --field=ID --format=ids) --post_title="$(echo $page | sed -e 's/^ *//' -e 's/ *$//')"
+# done
+
+# set pretty urls
+wp rewrite structure '/%postname%/' --hard
+wp rewrite flush --hard
+
+# delete akismet and hello dolly
+echo "Deleting default plugins..."
+wp plugin delete akismet
+wp plugin delete hello
+
+# delete default themes
+echo "Deleting all default themes..."
+wp theme delete twentyfifteen
+wp theme delete twentysixteen
+
+# install WordPress Plugins
+echo "Installing WordPress plugins..."
+WPPLUGINS=( custom-post-type-ui post-types-order svg-support timber-library advanced-custom-fields )
+wp plugin install ${WPPLUGINS[@]} --activate
+
+# Copy timber starter theme from plugin directory into themes directory and activate it
+echo "Installing & Activating Timber Starter Theme..."
+cp -r wp-content/plugins/timber-library/timber-starter-theme wp-content/themes/
+
+# install & activate the timber starter theme
+wp theme activate timber-starter-theme
+wp theme delete twentyseventeen
+
+# clear
+
+# create a navigation bar
+# wp menu create "Main Navigation"
+
+# add pages to navigation
+# export IFS=" "
+# for pageid in $(wp post list --order="ASC" --orderby="date" --post_type=page --post_status=publish --posts_per_page=-1 --field=ID --format=ids); do
+# 	wp menu item add-post main-navigation $pageid
+# done
+
+# assign navigaiton to primary location
+# wp menu location assign main-navigation primary
+
+# clear
+
+# install TeamDNL Webpack Starter Kit
+
+# NPM Install - check if yarn is installed if so use that, if not use NPM
+
+# run webpack dev server
+
+
+
+echo "================================================================="
+echo "Installation is complete. Your username/password is listed below."
+echo ""
+echo "Username: $wpuser"
+echo "Password: $password"
+echo ""
+echo "================================================================="
+
+
+
+fi
